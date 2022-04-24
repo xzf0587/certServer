12d7a1df2782f71bc904d579e240c068161cbfdc
Zhaofeng Xu <zhaofengxu@microsoft.com>
2022-04-24 15:34:04 +0800

    cert server

---
+++ index.js
@@ -0,0 +1,21 @@
+// import * as https from "https";
+// import * as fs from "fs-extra";
+const https = require("https");
+const fs = require("fs-extra")
+const serverCertAuthConfig = {
+    requestCert: true,
+    cert: fs.readFileSync('cert/server_cert.pem','utf8'),
+    key: fs.readFileSync('cert/server_key.pem','utf8'),
+    ca: fs.readFileSync('cert/server_cert.pem','utf8'),
+};
+
+const host = "localhost";
+const port = 53002;
+const server = https.createServer(serverCertAuthConfig, (req, res) => {
+    res.writeHead(200);
+    const cert = req.socket.getPeerCertificate();
+    res.end(cert.subject.CN);
+});
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
